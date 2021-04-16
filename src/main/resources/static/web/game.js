ba8a32ff134283bd1db6f4cdf22dcb6af58d2ab9
DESKTOP-SH9MK0E\dima <m.salischiker>
2021-04-15 23:59:39 -0300

    no message

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -20,71 +20,71 @@ fetch('http://localhost:8080/api/game_view/' + gp)
 
 
 var app = new Vue({
-            el: '#app',
-            data: {
-                gameView: [],
-                columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                rows: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
-                rows2: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-                currentPlayer: "",
-                currentAdversary: "",
-                OpponnentShots: [],
-                shipLocations: [],
-            },
-            methods: {
-                paintLocations: function () {
-                    for (x = 0; x < app.gameView.ships.length; x++) {
-                        for (i = 0; i < app.gameView.ships[x].locations.length; i++) {
-                            document.getElementById(app.gameView.ships[x].locations[i]).classList.toggle("ship");
+    el: '#app',
+    data: {
+        gameView: [],
+        columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        rows: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+        rows2: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+        currentPlayer: "",
+        currentAdversary: "",
+        OpponnentShots: [],
+        shipLocations: [],
+    },
+    methods: {
+        paintLocations: function () {
+            for (x = 0; x < app.gameView.ships.length; x++) {
+                for (i = 0; i < app.gameView.ships[x].locations.length; i++) {
+                    document.getElementById(app.gameView.ships[x].locations[i]).classList.toggle("ship");
 
-                            app.shipLocations.push(app.gameView.ships[x].locations[i]);
-                        }
+                    app.shipLocations.push(app.gameView.ships[x].locations[i]);
+                }
+            }
+
+        },
+        salvoLocations: function () {
+
+            var salvosPlayer = app.gameView.salvoes.filter(em => em.playerId == app.currentPlayer.id)
+            var salvosOpponnent = app.gameView.salvoes.filter(em => em.playerId != app.currentPlayer.id)
+
+            for (i = 0; i < salvosPlayer.length; i++) {
+                for (x = 0; x < salvosPlayer[i].locations.length; x++) {
+                    document.getElementById(salvosPlayer[i].locations[x].toLowerCase()).classList.toggle("salvo");
+                    document.getElementById(salvosPlayer[i].locations[x].toLowerCase()).innerHTML = salvosPlayer[i].turn;
+                }
+            }
+            for (i = 0; i < salvosOpponnent.length; i++) {
+                for (x = 0; x < salvosOpponnent[i].locations.length; x++) {
+                    document.getElementById(salvosOpponnent[i].locations[x]).classList.toggle("salvoAdversary");
+                    document.getElementById(salvosOpponnent[i].locations[x]).innerHTML = salvosOpponnent[i].turn;
+
+                    app.OpponnentShots.push(salvosOpponnent[i].locations[x]);
+                }
+            }
+
+        },
+        playerIndex: function () {
+            var g = parseInt(gp)
+            var gamer = app.gameView.gamePlayers.find(em => em.id == g)
+            var adversary = app.gameView.gamePlayers.find(em => em.id != g)
+            console.log("jugador actual " + gamer.player.email)
+            app.currentPlayer = gamer.player;
+            app.currentAdversary = adversary.player;
+        },
+
+        hittedShips: function () {
+
+            for (x = 0; x < app.OpponnentShots.length; x++) {
+                for (i = 0; i < app.shipLocations.length; i++) {
+                    if (app.shipLocations[i] == app.OpponnentShots[x]) {
+                        //document.getElementById(app.OpponnentShots[x]).innerHTML = "X";
+                        document.getElementById(app.OpponnentShots[x]).classList.toggle("salvo");
+                        var turn = document.getElementById(app.OpponnentShots[x]).innerHTML
+                        console.log("hitted!: " + app.OpponnentShots[x] + ", on turn: " + turn)
                     }
+                }
+            }
+        },
 
-                },
-                salvoLocations: function () {
-
-                    var salvosPlayer = app.gameView.salvoes.filter(em => em.playerId == app.currentPlayer.id)
-                    var salvosOpponnent = app.gameView.salvoes.filter(em => em.playerId != app.currentPlayer.id)
-
-                    for (i = 0; i < salvosPlayer.length; i++) {
-                        for (x = 0; x < salvosPlayer[i].locations.length; x++) {
-                            document.getElementById(salvosPlayer[i].locations[x].toLowerCase()).classList.toggle("salvo");
-                            document.getElementById(salvosPlayer[i].locations[x].toLowerCase()).innerHTML = salvosPlayer[i].turn;
-                        }
-                    }
-                    for (i = 0; i < salvosOpponnent.length; i++) {
-                        for (x = 0; x < salvosOpponnent[i].locations.length; x++) {
-                            document.getElementById(salvosOpponnent[i].locations[x]).classList.toggle("salvoAdversary");
-                            document.getElementById(salvosOpponnent[i].locations[x]).innerHTML = salvosOpponnent[i].turn;
-                            
-                            app.OpponnentShots.push(salvosOpponnent[i].locations[x]);
-                        }
-                    }
-
-                },
-                playerIndex: function () {
-                    var g = parseInt(gp)
-                    var gamer = app.gameView.gamePlayers.find(em => em.id == g)
-                    var adversary = app.gameView.gamePlayers.find(em => em.id != g)
-                    console.log("jugador actual " + gamer.player.email)
-                    app.currentPlayer = gamer.player;
-                    app.currentAdversary = adversary.player;
-                },
-
-                hittedShips: function () {
-
-                    for (x = 0; x < app.OpponnentShots.length; x++) {
-                        for (i = 0; i < app.shipLocations.length; i++) {
-                            if (app.shipLocations[i] == app.OpponnentShots[x]) {
-                                //document.getElementById(app.OpponnentShots[x]).innerHTML = "X";
-                                document.getElementById(app.OpponnentShots[x]).classList.toggle("salvo");
-                                var turn = document.getElementById(app.OpponnentShots[x]).innerHTML
-                                console.log("hitted!: " + app.OpponnentShots[x] + ", on turn: " + turn )
-                            }
-                        }
-                    }
-                },
-
-            },
+    },
 })
--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -3,6 +3,45 @@ var app = new Vue({
     data: {
         games: [],
         scores: [],
+    },
+    methods: {
+        scorePlayers: function (games) {
+            for (i = 0; i < games.length; i++) {
+                var gameplayers = games[i].gamePlayers
+                for (j = 0; j < gameplayers.length; j++) {
+
+                    //app.scores inicialmente esta vacio.. pero al realizar el primer for un ciclo completo ya hay valores pusheados en app.score
+                    //y para ese caso que el app.score no tiene valores aun, para eso es el primer if que setea todo a un 0 default
+                    var playerIndex = app.scores.findIndex(scorePlayer => scorePlayer.player === gameplayers[j].player.email)
+
+                    if (gameplayers[j].score != null) {
+                        if (playerIndex == -1) {
+                            var Score = {
+                                player: gameplayers[j].player.email,
+                                lost: 0,
+                                tied: 0,
+                                wins: 0,
+                                total: 0,
+                            };
+                            if (gameplayers[j].score == 0.0) Score.lost++;
+                            else if (gameplayers[j].score == 0.5) Score.tied++;
+                            else if (gameplayers[j].score == 1.0) Score.wins++;
+                            Score.total += gameplayers[j].score;
+                            app.scores.push(Score);
+
+                        } else {
+                            if (gameplayers[j].score == 0.0) app.scores[playerIndex].lost++;
+                            else if (gameplayers[j].score == 0.5) app.scores[playerIndex].tied++;
+                            else if (gameplayers[j].score == 1.0) app.scores[playerIndex].wins++;
+                            app.scores[playerIndex].total += gameplayers[j].score;
+
+
+                        }
+                    }
+                }
+            }
+            console.log(app.scores);
+        }
     }
 })
 
@@ -14,45 +53,5 @@ fetch('http://localhost:8080/api/games')
 
         app.games = data;
         console.log(app.games);
-        scorePlayers(app.games);
+        app.scorePlayers(app.games);
     })
-
-
-
-function scorePlayers(games) {
-    for (i = 0; i < games.length; i++) {
-        var gameplayers = games[i].gamePlayers
-        for (j = 0; j < gameplayers.length; j++) {
-
-            //app.scores inicialmente esta vacio.. pero al realizar el primer for un ciclo completo ya hay valores pusheados en app.score
-            //y para ese caso que el app.score no tiene valores aun, para eso es el primer if que setea todo a un 0 default
-            var playerIndex = app.scores.findIndex(scorePlayer => scorePlayer.player === gameplayers[j].player.email)
-
-            if (gameplayers[j].score != null) {
-                if (playerIndex == -1) {
-                    var Score = {
-                        player: gameplayers[j].player.email,
-                        lost: 0,
-                        tied: 0,
-                        wins: 0,
-                        total: 0,
-                    };
-                    if (gameplayers[j].score == 0.0) Score.lost++;
-                    else if (gameplayers[j].score == 0.5) Score.tied++;
-                    else if (gameplayers[j].score == 1.0) Score.wins++;
-                    Score.total += gameplayers[j].score;
-                    app.scores.push(Score);
-
-                } else {
-                    if (gameplayers[j].score == 0.0) app.scores[playerIndex].lost++;
-                    else if (gameplayers[j].score == 0.5) app.scores[playerIndex].tied++;
-                    else if (gameplayers[j].score == 1.0) app.scores[playerIndex].wins++;
-                    app.scores[playerIndex].total += gameplayers[j].score;
-
-                
-            }
-        }
-    }
-}
-console.log(app.scores);
-}
